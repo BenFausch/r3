d6655b19b1ce9b696cafa2f6783680d6bdd008e4
Ben Fausch <ben@digitalbureau.com>
2018-10-08 14:12:42 -0600

    built app, added linting etc

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import renderHTML from 'react-render-html';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {posts: {}};
+    }
+    callApi() {
+        fetch('https://thewirecutter.com/wp-json/wp/v2/posts')
+            .then((result) => {
+                return result.json();
+            }).then((jsonResult) => {
+                console.log(jsonResult);
+                this.setState({posts: jsonResult});
+            });
+    }
+
+    postBody(k, v) {
+        var pst = this.state.posts;
+        var container = [];
+        for (var key in pst) {
+            if (pst[key]) {
+                var body = renderHTML(pst[key]['content']['rendered']);
+                container.push(<div key={pst[key]['id']}><h3>{pst[key]['title']['rendered']}</h3><div>{body}</div></div>);
+            }
+        }
+    
+        return container;
+    }
+
+    render() {
+    
+        return (
+            <div className="App">
+                <button onClick={() => this.callApi()}>
+                Click here! I pull API data!
+                </button>
+                <div>{this.postBody()}</div>
+
+            </div>
+        );
+    }
 }
 
 export default App;
+
+
